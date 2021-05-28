425538460dfec2a692ee1838e42b47e5da393ca6
Arslon Saidov <arslonbeksaidov@users.noreply.github.com>
2021-05-28 11:45:17 +0500

    decorator function examples
    You can learn python decorators  from this examples

---
+++ decorators.py
@@ -0,0 +1,118 @@
+# simple decorator with args
+def decorator_with_arguments(function):
+    def wrapper_accepting_arguments(arg1, arg2):
+        print("My arguments are: {0}, {1}".format(arg1.upper(),arg2.upper()))
+        function(arg1, arg2)
+    return wrapper_accepting_arguments
+
+
+@decorator_with_arguments
+def cities(city_one, city_two):
+    print("Cities I love are {0} and {1}".format(city_one, city_two))
+
+cities("Nairobi", "Accra")
+
+
+#-----------------------------------------------------------------------------------
+
+# simple decorator without args and kwargs
+def a_decorator_passing_arbitrary_arguments(function_to_decorate):
+    def a_wrapper_accepting_arbitrary_arguments(*args,**kwargs):
+        print('The positional arguments are', args)
+        print('The keyword arguments are', kwargs)
+        function_to_decorate(*args)
+    return a_wrapper_accepting_arbitrary_arguments
+
+@a_decorator_passing_arbitrary_arguments
+def function_with_no_argument():
+    print("No arguments here.")
+
+function_with_no_argument()
+#---------------------------------------------------------------------------------------------
+#simple decorator with args
+def a_decorator_passing_arbitrary_arguments(function_to_decorate):
+    def a_wrapper_accepting_arbitrary_arguments(*args,**kwargs):
+        print('The positional arguments are', args)
+        print('The keyword arguments are', kwargs)
+        function_to_decorate(*args)
+    return a_wrapper_accepting_arbitrary_arguments
+
+
+@a_decorator_passing_arbitrary_arguments
+def function_with_arguments(a, b, c):
+    print(a, b, c)
+
+function_with_arguments(1,2,3)
+#------------------------------------------------------------------------------------------------
+# simple decorator with kwargs
+def a_decorator_passing_arbitrary_arguments(function_to_decorate):
+    def a_wrapper_accepting_arbitrary_arguments(*args,**kwargs):
+        print('The positional arguments are', args)
+        print('The keyword arguments are', kwargs)
+        function_to_decorate(*args)
+    return a_wrapper_accepting_arbitrary_arguments
+
+
+@a_decorator_passing_arbitrary_arguments
+def function_with_keyword_arguments():
+    print("This has shown keyword arguments")
+
+function_with_keyword_arguments(first_name="Derrick", last_name="Mwiti")
+#---------------------------------------------------------------------------------------------------
+# to find wrapper function name 
+def a_decorator_passing_arbitrary_arguments(function_to_decorate):
+    def a_wrapper_accepting_arbitrary_arguments(*args,**kwargs):
+        print('The positional arguments are', args)
+        print('The keyword arguments are', kwargs)
+        function_to_decorate(*args)
+    return a_wrapper_accepting_arbitrary_arguments
+
+
+@a_decorator_passing_arbitrary_arguments
+def function_with_keyword_arguments():
+    print("This has shown keyword arguments")
+
+print(function_with_keyword_arguments.__name__)
+#-----------------------------------------------------------------------------------------------
+# Advanced usage of decorator
+def decorator_maker_with_arguments(decorator_arg1, decorator_arg2, decorator_arg3):
+    def decorator(func):
+        def wrapper(function_arg1, function_arg2, function_arg3) :
+            "This is the wrapper function"
+            print("The wrapper can access all the variables\n"
+                  "\t- from the decorator maker: {0} {1} {2}\n"
+                  "\t- from the function call: {3} {4} {5}\n"
+                  "and pass them to the decorated function"
+                  .format(decorator_arg1, decorator_arg2,decorator_arg3,
+                          function_arg1, function_arg2,function_arg3))
+            return func(function_arg1, function_arg2,function_arg3)
+
+        return wrapper
+
+    return decorator
+
+pandas = "Pandas"
+@decorator_maker_with_arguments(pandas, "Numpy","Scikit-learn")
+def decorated_function_with_arguments(function_arg1, function_arg2,function_arg3):
+    print("This is the decorated function and it only knows about its arguments: {0}"
+           " {1}" " {2}".format(function_arg1, function_arg2,function_arg3))
+
+decorated_function_with_arguments(pandas, "Science", "Tools")
+
+# result below
+#The wrapper can access all the variables
+#    - from the decorator maker: Pandas Numpy Scikit-learn
+#    - from the function call: Pandas Science Tools
+#and pass them to the decorated function
+#This is the decorated function, and it only knows about its arguments: Pandas Science Tools
+
+
+
+
+
+
+
+
+
+
+
